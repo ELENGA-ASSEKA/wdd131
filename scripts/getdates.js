3fbd1c16190ddade7440b0314dbeefdcc572b1ca
= <=>
2024-09-24 06:37:14 -0700

    push

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,5 @@
+const year = new Date().getFullYear();
 
+document.getElementById("currentyear").innerHTML = `&copy; ${year}`
+
+document.getElementById("lastModified").textContent = `last Modification: ${document.lastModified}`
