b599845acc5978d2a9ae8df68e3848f3168538e5
= <=>
2024-09-30 05:45:37 -0700

    git push

---
+++ scripts/train.js
@@ -0,0 +1,8 @@
+let prenom = "Je m'appelle Pierre";
+let age = 29;
+let age2 = '29';
+
+
+document.getElementById('p1').innerHTML = 'Type de pronom:' + typeof prenom;
+document.getElementById('p2').innerHTML = ' Type de age:' + typeof age;
+document.getElementById('p3').innerHTML = 'Type de age2:' + typeof age2;
