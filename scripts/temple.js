81ae176564248ebe6d16b0ecba112471a2bf09ea
= <=>
2024-10-05 04:06:56 -0700

    push

---
+++ scripts/temple.js
@@ -0,0 +1,3 @@
+const year = new Date().getFullYear();
+document.getElementById('currentyear').innerHTML = `&copy; ${year}`;
+document.getElementById('lastModified').textContent = `last Modification: ${document.lastModified}`;
