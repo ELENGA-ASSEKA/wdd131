aea4fe850f27b64bd177a7c5c9b9aa0d40268b55
= <=>
2024-10-05 17:05:07 -0700

    push

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,3 +1,43 @@
+/*To display the current year*/ 
 const year = new Date().getFullYear();
 document.getElementById('currentyear').innerHTML = `&copy; ${year}`;
+
+/*To display the modification year*/
 document.getElementById('lastModified').textContent = `last Modification: ${document.lastModified}`;
+
+/*display the html elements*/
+
+
+const navLinks = document.getElementById('nav-links');
+const hamburger = document.getElementById('hamburger');
+
+/*Add Event listner */
+
+hamburger.addEventListener('click', () =>{ navLinks.classList.toggle('active');
+    console.log("Hamburger button clicked");
+    hamburger.textContent = navLinks.classList.contains('active') ? '✖' :'☰';
+
+});
+
+/*masque the mobile view */
+const mediaQuery = window.matchMedia('(max-width: 610px)');
+if (mediaQuery.matches) {
+    navLinks.style.display = 'none';
+    hamburger.style.display = 'block';
+}
+else {
+    navLinks.style.display = 'flex';
+    hamburger.style.display = 'none';
+};
+
+mediaQuery.addListener((e) =>{
+if (e.matches){
+    navLinks.style.display = 'none';
+    hamburger.style.display = 'block';
+}
+
+else{
+    navLinks.style.display =  'flex';
+    hamburger.style.display = 'none';
+}
+});
