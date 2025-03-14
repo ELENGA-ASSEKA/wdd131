48ba3623aa7282cf05c033d8dc9d827e5119dfa4
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-03-14 05:02:23 -0700

    push

---
+++ scripts/temples.js
@@ -0,0 +1,4 @@
+
+const year = new Date().getFullYear();
+document.getElementById('year').innerHTML =`&copy; ${year}`;
+document.getElementById('lastModified').innerHTML=`Last Modification: ${document.lastModified}`;
