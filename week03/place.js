88a3edecd4d1f14fec6bdc862d6621599033e8a0
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-03-25 02:48:53 -0700

    push

---
+++ week03/place.js
@@ -0,0 +1,4 @@
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = `&copy; ${year} `;
+
+document.getElementById("lastModified").innerHTML = `LastModification: ${document.lastModified}`
