54588727abb58fc42519958af46a58995f4fb491
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-03-26 13:12:06 -0700

    p

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,6 @@ const year = new Date().getFullYear();
 document.getElementById('year').innerHTML =`&copy; ${year}`;
 document.getElementById('lastModified').innerHTML=`Last Modification: ${document.lastModified}`;
 
-
 function toggle(){
    let menu = document.getElementById('menu');
 
@@ -16,17 +15,15 @@ function toggle(){
 }
 
 /*menu button js */
-
 const menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", () =>{
    menuBtn.classList.toggle("active");
-});
-
+})
 
 /*menu active*/
 const menu = document.querySelector(".menu");
 menuBtn.addEventListener("click", () => {
    menuBtn.classList.toggle("active");
    menu.classList.toggle("active");
-});
+})
 
