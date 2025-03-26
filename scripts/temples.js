14e1a66383f4400f565c9f8c71439376e2e7a3c5
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-03-26 12:25:40 -0700

    push

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,7 +7,7 @@ document.getElementById('lastModified').innerHTML=`Last Modification: ${document
 function toggle(){
    let menu = document.getElementById('menu');
 
-   if (menu.style.display === "block") {
+   if (menu.style.display === "block"){
       menu.style.display= "none";
    }
         else{
