ced83a507b8f189b68b49b759ec0b535a0b3f7c1
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-04-12 13:49:40 -0700

    p

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,4 @@
 const year = new Date().getFullYear();
 document.getElementById("year").innerHTML = `&copy; ${year}`;
 document.getElementById("lastModified").textContent= `lastModification: ${document.lastModified}`;
+
