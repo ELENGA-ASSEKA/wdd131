56d716dc26f1a969fae4577f180e55db34a20fbb
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-04-16 17:06:17 -0700

    p

--- project/script/about-us.js
+++ project/script/about-us.js
@@ -1,3 +1,11 @@
 const year = new Date().getFullYear();
 document.getElementById("year").innerHTML = `&copy; ${year}`;
 document.getElementById("lastModified").textContent= `lastModification: ${document.lastModified}`;
+
+let functionLink = document.getElementById("showhide");
+
+let swichedSection = document.getElementById("main");
+
+functionLink.addEventListener("click", () => {
+    swichedSection.classList.toggle(switced)
+})
